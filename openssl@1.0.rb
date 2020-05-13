105c078c9758e9a1b30f8a74731b7fc7f504f3b9
Martijn Pieters <mj@zopatista.com>
2020-05-13 15:33:24 +0100

    Update formula to mirror homebrew-core/openssl@1.1

--- openssl@1.0.rb
+++ openssl@1.0.rb
@@ -1,4 +1,4 @@
-# This formula tracks 1.0.2 branch of OpenSSL, not the 1.1.0 branch. 
+# This formula tracks 1.0.2 branch of OpenSSL, not the 1.1.0 branch.
 # It was removed from homebrew-core in November 2019, see
 # https://github.com/Homebrew/homebrew-core/pull/46876
 # because OpenSSL 1.0.x reached EOL on 2019-12-31
@@ -12,18 +12,10 @@ class OpensslAT10 < Formula
   mirror "https://www.mirrorservice.org/sites/ftp.openssl.org/source/openssl-1.0.2t.tar.gz"
   sha256 "14cb464efe7ac6b54799b34456bd69558a749a4931ecfd9cf9f71d7881cac7bc"
 
-  keg_only :provided_by_macos,
-    "Apple has deprecated use of OpenSSL in favor of its own TLS and crypto libraries"
+  keg_only :shadowed_by_macos, "macOS provides LibreSSL"
 
-  def install
-    # OpenSSL will prefer the PERL environment variable if set over $PATH
-    # which can cause some odd edge cases & isn't intended. Unset for safety,
-    # along with perl modules in PERL5LIB.
-    ENV.delete("PERL")
-    ENV.delete("PERL5LIB")
-
-    ENV.deparallelize
-    args = %W[
+  def configure_args
+    %W[
       --prefix=#{prefix}
       --openssldir=#{openssldir}
       no-ssl2
@@ -31,10 +23,22 @@ class OpensslAT10 < Formula
       no-zlib
       shared
       enable-cms
-      darwin64-x86_64-cc
-      enable-ec_nistp_64_gcc_128
     ]
-    system "perl", "./Configure", *args
+  end
+
+  def install
+    # This could interfere with how we expect OpenSSL to build.
+    ENV.delete("OPENSSL_LOCAL_CONFIG_DIR")
+
+    # This ensures where Homebrew's Perl is needed the Cellar path isn't
+    # hardcoded into OpenSSL's scripts, causing them to break every Perl update.
+    # Whilst our env points to opt_bin, by default OpenSSL resolves the symlink.
+    ENV["PERL"] = Formula["perl"].opt_bin/"perl" if which("perl") == Formula["perl"].opt_bin/"perl"
+
+    arch_args = %w[darwin64-x86_64-cc enable-ec_nistp_64_gcc_128]
+
+    ENV.deparallelize
+    system "perl", "./Configure", *(configure_args + arch_args)
     system "make", "depend"
     system "make"
     system "make", "test"
@@ -42,7 +46,7 @@ class OpensslAT10 < Formula
   end
 
   def openssldir
-    etc/"openssl"
+    etc/"openssl@1.0"
   end
 
   def post_install
@@ -56,7 +60,7 @@ class OpensslAT10 < Formula
     )
 
     valid_certs = certs.select do |cert|
-      IO.popen("#{bin}/openssl x509 -inform pem -checkend 0 -noout", "w") do |openssl_io|
+      IO.popen("#{bin}/openssl x509 -inform pem -checkend 0 -noout >/dev/null", "w") do |openssl_io|
         openssl_io.write(cert)
         openssl_io.close_write
       end
@@ -68,10 +72,10 @@ class OpensslAT10 < Formula
     (openssldir/"cert.pem").atomic_write(valid_certs.join("\n") << "\n")
   end
 
-  def caveats; <<~EOS
-    A CA file has been bootstrapped using certificates from the SystemRoots
-    keychain. To add additional certificates (e.g. the certificates added in
-    the System keychain), place .pem files in
+  def caveats
+    <<~EOS
+      A CA file has been bootstrapped using certificates from the system
+      keychain. To add additional certificates, place .pem files in
       #{openssldir}/certs
 
     and run
@@ -81,13 +85,13 @@ class OpensslAT10 < Formula
 
   test do
     # Make sure the necessary .cnf file exists, otherwise OpenSSL gets moody.
-    assert_predicate HOMEBREW_PREFIX/"etc/openssl/openssl.cnf", :exist?,
+    assert_predicate pkgetc/"openssl.cnf", :exist?,
             "OpenSSL requires the .cnf file for some functionality"
 
     # Check OpenSSL itself functions as expected.
     (testpath/"testfile.txt").write("This is a test file")
     expected_checksum = "e2d0fe1585a63ec6009c8016ff8dda8b17719a637405a4e23c0ff81339148249"
-    system "#{bin}/openssl", "dgst", "-sha256", "-out", "checksum.txt", "testfile.txt"
+    system bin/"openssl", "dgst", "-sha256", "-out", "checksum.txt", "testfile.txt"
     open("checksum.txt") do |f|
       checksum = f.read(100).split("=").last.strip
       assert_equal checksum, expected_checksum
